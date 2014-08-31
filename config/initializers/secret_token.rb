6b33ac7a6f9abe6b1fd2a527be9d08b669605bb0
Nathan Hall <nathan.mh@gmail.com>
2014-08-31 13:38:51 -0500

    redo of generocity, start from scratch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeneroCity::Application.config.secret_key_base = 'b3128dcc4b15f225fe7cf817050c6beeee7561dc59ede9bb6ad0852e265ce07305d51cfa3cdb9c52877aceccf4816e7c1bbf98f03f6ad6af42bd06266949a19e'
